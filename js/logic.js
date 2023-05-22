18bbdbd54817b601bd128a28cfb1495309da15b0
rperroni <rperroni@fi.uba.ar>
2023-05-22 19:20:14 -0300

    Mejora mostrando el total

--- js/logic.js
+++ js/logic.js
@@ -15,8 +15,10 @@ document.getElementById('btn-total').addEventListener('click', function (event) 
     let numberOfTickets = document.getElementById('amount-of-tickets').value;
     let discountType = CATEGORIAS_DE_DESCUENTO[document.getElementById('discount-type').value];
     document.getElementById("total-value").innerHTML = "Total a pagar: $" + totalValue(numberOfTickets, discountType);
+
+    document.getElementById("total-value").style.display = "block";
   });
 
 document.getElementById('btn-clear').onclick = function () {
-    document.getElementById("total-value").innerHTML = "Total a pagar: $"
+    document.getElementById("total-value").style.display = "none";
 };
